13d0054b3467b52e30bd46bffc5f61cc1ae94845
user02 <user02@abc.com>
2024-09-26 11:10:42 +0900

    modify test03.py by uesr01

--- test03.py
+++ test03.py
@@ -1,7 +1,9 @@
 #test03.py
 
 def sayHello(name):
-    return 'Hello, ' + name
+    return 'Hello, ' + name + '!!!!!!!' #user01수정
     
 inName = Tom
 print(sayHello(inName))
+
+print('Requestes by boss') #추가 user01 추가
